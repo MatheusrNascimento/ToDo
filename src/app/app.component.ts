c0f1af2dff6543047fa1c55a5a49547a475a1e9b
Matheus Rodrigues <matheusrnascimento92@gmail.com>
2024-12-04 23:05:39 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'ToDo';
-}
+
+export class AppComponent implements OnInit {
+  title = 'Tarefas do Dia';
+  todos: any[] = [];
+
+  ngOnInit(): void {
+    this.todos.push('Passear com o gato');
+    this.todos.push('Ir ao mercado');
+    this.todos.push('Sair para jantar');
+  }
+};
