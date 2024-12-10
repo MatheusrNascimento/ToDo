01208fa1b4677a488363166f7bdd5c4b61b19429
Matheus Rodrigues <matheusrnascimento92@gmail.com>
2024-12-09 23:50:32 -0300

    feat

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,47 @@
 import { Component, OnInit, } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Todo } from '../models/todo.model';
+import {FormBuilder, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'Tarefas do Dia';
-  todos: any[] = [];
+  todos: Todo[] = [];
+  form!: FormGroup;
 
-  ngOnInit(): void {
-    this.todos.push('Passear com o gato');
-    this.todos.push('Ir ao mercado');
-    this.todos.push('Sair para jantar');
+  constructor(private fb: FormBuilder)
+  {
+    this.form = this.fb.group({
+      title: ['',
+        [Validators.minLength(3),
+        Validators.maxLength(60),
+        Validators.required]]
+    });
+    this.todos.push(new Todo(1, 'andar com a ceci', false));
+    this.todos.push(new Todo(2, 'almoçar', false));
+    this.todos.push(new Todo(3, 'comprar lanche na segunda', true));
   }
+
+  remove(todo: Todo){
+    const index = this.todos.indexOf(todo);
+    if (index !== -1)
+      this.todos.splice(index, 1);
+  }
+
+  markAsDone(todo: Todo){
+    todo.done = true;
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+  }
+
+  protected readonly FormBuilder = FormBuilder;
 };
